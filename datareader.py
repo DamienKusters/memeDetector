0cecbcc84c00e0c7936bb7afec2f0781c2e66821
DamienKusters <damien.kusters@ziggo.nl>
2020-04-28 22:54:54 +0200

    Backup init

---
+++ datareader.py
@@ -0,0 +1,16 @@
+import torchvision
+import torch
+
+
+def load_dataset(path):
+    train_dataset = torchvision.datasets.ImageFolder(
+        root=path,
+        transform=torchvision.transforms.ToTensor()
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=64,
+        num_workers=0,
+        shuffle=True
+    )
+    return train_loader
---
+++ main.py
@@ -0,0 +1,68 @@
+from __future__ import print_function, division
+from PIL import Image
+from modeltrainer import train_model
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+
+
+
+alexnet = models.alexnet(pretrained=True)
+num_c = 4096 # should be 4096 (based on alexnet source code)
+
+alexnet.classifier[-1] = nn.Linear(num_c, 2) #Replace last (index: -1) final fully connected layer with our own, in this case with one output 
+
+#alexnet = alexnet.to(device) #Move to GPU/CPU if needed
+alexnet.cuda()
+
+criterion = nn.MSELoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(alexnet.parameters(), lr=0.001, momentum=0.9) #Optimizer crap I practically understand but not atm.
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1) #set training rate, just dont train TOO much
+
+alexnet = train_model(alexnet, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+
+transform = transforms.Compose([        #Here we are defining a variable transform which is a combination of all the image transformations to be carried out on the input image.
+    transforms.Resize(256),             #Resize the image to 256×256 pixels.
+    transforms.CenterCrop(224),         #Crop the image to 224×224 pixels about the center.
+    transforms.ToTensor(),              #Convert the image to PyTorch Tensor data type.
+    transforms.Normalize(               #Normalize the image by setting its mean and standard deviation to the specified values.
+        mean=[0.485, 0.456, 0.406],     
+        std=[0.229, 0.224, 0.225]       
+    )
+])
+
+
+
+
+img = Image.open("./yel.jpg")
+img_t = transform(img)
+batch_t = torch.unsqueeze(img_t, 0)
+
+
+alexnet.eval()#put the model in evaluation mode
+out = alexnet(batch_t)
+#print(out.shape)
+
+
+with open('./imagenet_classes.txt') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+
+_, index = torch.max(out, 1)
+percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+print(labels[index[0]], percentage[index[0]].item())
+
+
+
+
+
